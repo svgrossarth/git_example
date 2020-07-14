6f232ad941981bd434871db7291eac27f92b0965
svgrossarth <svgrossarth@ucdavis.edu>
2020-07-13 17:41:47 -0700

    initial commit

---
+++ git_example/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  git_example
+//
+//  Created by Spencer Grossarth on 7/13/20.
+//  Copyright © 2020 Spencer Grossarth. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
